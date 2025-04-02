46d91275fc9e64047ddd37c0ac678cd4031875b7
Mingcong Bai <jeffbai@aosc.io>
2025-04-02 15:21:26 +0800

    fix(examples): add missing external definition for early_printf

--- examples/loongson_2k0300/src/cache_stage.c
+++ examples/loongson_2k0300/src/cache_stage.c
@@ -2,7 +2,9 @@
 #include "ls2k0300.h"
 #include "bonito.h"
 
-#define  pr_info(fmt, args...) early_printf(fmt, ##args)
+extern int early_printf(const char *fmt, ...);
+
+#define pr_info(fmt, args...) early_printf(fmt, ##args)
 void main(unsigned long long msize)
 {
 	pr_info("mem init done!\n");
