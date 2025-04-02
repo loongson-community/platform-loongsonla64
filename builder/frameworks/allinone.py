70e99d7a50a5f7ae6c40e2a9af16477f0593ff7b
Mingcong Bai <jeffbai@aosc.io>
2025-04-02 17:46:35 +0800

    chore(frameworks): drop a useless comment about Lafk

--- builder/frameworks/allinone.py
+++ builder/frameworks/allinone.py
@@ -12,12 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-Lafk
-
-https://www.loongson.com
-"""
-
 import sys
 from os.path import join, isfile
 
